111c8ea8f930a92a4be8601af22d4870f4be131a
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2025-04-13 23:07:24 -0400

    require newline between let and in
    Signed-off-by: Ihar Hrachyshka <ihar.hrachyshka@gmail.com>

---
+++ tests/tlpdb.nix
@@ -0,0 +1,26 @@
+{
+  collection-langczechslovak = {
+    revision = 54074;
+    shortdesc = "Czech/Slovak";
+    stripPrefix = 0;
+    deps = [
+      "babel-czech"
+      "babel-slovak"
+      "cnbwp"
+      "collection-basic"
+      "collection-latex"
+      "cs"
+      "csbulletin"
+      "cslatex"
+      "csplain"
+      "cstex"
+      "hyphen-czech"
+      "hyphen-slovak"
+      "lshort-czech"
+      "lshort-slovak"
+      "texlive-cz"
+      "vlna"
+    ];
+    hasCatalogue = false;
+  };
+}
